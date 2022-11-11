7ed431761b2309813beccba51e403b522b415cbe
Raihan-2k <muh.rn07@gmail.com>
2022-11-11 12:25:39 +0700

    update

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'f38cf22c14b461f3da8ca004330d165d',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
